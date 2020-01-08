0ec463ea891010b41537cc82519737292c39cb8f
Ben Cardoen <bcardoen@sfu.ca>
2020-01-08 08:03:53 -0800

    Remote mount script, license

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# labtools
+# A set of adminstration tools to make working with remote *nix systems easier
 
---
+++ scripts/remotemount.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#@author Ben Cardoen, Ghassan Hamarneh
+#Remote mounts Cedar home drive
+
+# Uncomment if you need to jump between systems
+#JUMP="-o ssh_command='ssh -J <you>@<intermediate_host>'"
+REMOTE="cedar.computecanada.ca"
+#Change if your user id is different
+REMOTEUSER=$USER
+#Change to a different mount point
+MOUNTPOINT="/home/bcardoen/mountcedar"
+OPTIONS="-C -o follow_symlinks -o cache=yes -o reconnect -o cache_timeout=300 -o kernel_cache"
+# If you need to jump
+# OPTIONS='-C -o ssh_command='''ssh -J <you>@<intermediate>''
+
+#Unmount Linux
+UMOUNTCMD="fusermount3 -u $MOUNTPOINT"
+#Unmount OSX
+#UMOUNTCMD="umount $MOUNTPOINT"
+
+echo "Mounting remote file systems @ $REMOTE to $MOUNTPOINT ... with options $OPTIONS"
+sshfs $REMOTE:/home/$REMOTEUSER $MOUNTPOINT $OPTIONS
+if [ $? -eq 0 ]; then
+	echo "Mount succesfull"
+else
+	echo "Mount failed"
+	echo "Trying umount to resolve old mounts ..."
+	$UMOUNTCMD
+	if [ $? -eq 0 ]; then
+		echo "Force unmount successful, trying to mount again ..."
+		sshfs $REMOTE:/home/$REMOTEUSER $MOUNTPOINT $OPTIONS
+		if [ $? -eq 0 ]; then
+			echo "... Mount successful"
+		else
+			echo "... Failed, giving up...."
+		fi
+	else
+		echo "... Force unmount failed, giving up"
+	fi
+fi
+echo "Done"
