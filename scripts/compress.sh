30fb6a8819c05215beb4cd3a2d4ffc93c2eee5cf
Ben Cardoen <bencardoen@gmail.com>
2021-03-24 12:06:50 -0700

    Make compression script more robust

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -euo pipefail
+if (( $# != 3 )); then
+    >&2 echo "Expected usage : $./compress.sh SOURCEFOLDER TARGETARCHIVE NUMPROCESSORS"
+fi
 SOURCE=$1 # First argument is source directory (/home/important/stuff/)
 TARGET=$2 # Second argument is the archive ('mystuf.tar.gz')
 PROCESSORS=$3 # Nr of processors to use (4-8 is the sweet spot)
