281cdb07fe39262eab5fcecde8d37fab0da5a237
bencardoen <bcardoen@sfu.ca>
2021-07-22 21:09:59 +0000

    Update compress.sh

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,10 +1,24 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Copyright 2021, Ben Cardoen
 #!/bin/bash
 set -euo pipefail
-if (( $# != 3 )); then
-    >&2 echo "Expected usage : $./compress.sh SOURCEFOLDER TARGETARCHIVE NUMPROCESSORS"
+if (( $# != 4 )); then
+    >&2 echo "Expected usage : $./compress.sh SOURCEFOLDER TARGETARCHIVE NUMPROCESSORS REGEXTOSKIP"
 fi
-SOURCE=$1 # First argument is source directory (/home/important/stuff/)
-TARGET=$2 # Second argument is the archive ('mystuf.tar.gz')
-PROCESSORS=$3 # Nr of processors to use (4-8 is the sweet spot)
-echo "Archiving $SOURCE into $TARGET using $PROCESSORS parallel processes"
-tar cf - $SOURCE  -P | pv -s $(du -sb $SOURCE | awk '{print $1}') | pigz -p $PROCESSORS > $TARGET
+INDIR=$1
+ARCHIVE=$2
+CORES=$3
+EXCLUDEREGEX=$4
+
+tar --exclude=${EXCLUDEREGEX} --use-compress-program="pigz -p $CORES --best --recursive" -c --checkpoint-action=ttyout='%{%Y-%m-%d %H:%M:%S}t (%d sec): #%u, %T%*\r' $INDIR -f $ARCHIVE 
