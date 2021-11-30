3a1391dfbe521df5674b473cb606764c2104d6aa
bencardoen <26388277+bencardoen@users.noreply.github.com>
2021-11-30 20:22:07 +0000

    Update script with authorship + license

--- scripts/remotemount.sh
+++ scripts/remotemount.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-#@author Ben Cardoen, Ghassan Hamarneh
+#@author Ben Cardoen
 #Remote mounts Cedar home drive
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 # Uncomment if you need to jump between systems
 #JUMP="-o ssh_command='ssh -J <you>@<intermediate_host>'"
